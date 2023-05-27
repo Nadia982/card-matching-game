1d4a1fe220347cbc68974fee1ce8ea0d7aad253b
Natalie Gillam <ngillam@hotmail.co.uk>
2023-05-27 11:00:32 +0100

    creating the card grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/pexels-jill-burrow-6858608.jpg" },
+  { src: "/img/pexels-luis-lima-10514728.jpg" },
+  { src: "/img/pexels-oliver-sjöström-1078981.jpg" },
+  { src: "/img/pexels-tobias-bjørkli-1900203.jpg" },
+  { src: "/img/pexels-roman-odintsov-6798334.jpg" },
+  { src: "/img/pexels-todd-trapani-1420440.jpg" },
 ];
 
 function App() {
@@ -30,6 +30,14 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card)=>(
+          <div className="card" key={card.id}>
+            <img className="front" alt={card.src} src={card.src}/>
+            <img src="/img/pexels-jonny-lew-1121123.jpg" className="back" alt="card back"/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
